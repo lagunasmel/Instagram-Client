f3e197eb6a53a0a4c4348337657466f6b6ee649f
Melissa Lagunas <lagunasmel@gmail.com>
2021-03-17 11:15:43 -0400

    Implemented signing up and logging in

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // --- Copy this only
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "JuNvRjFGLZe3hyDmBGMddoBElWi0yxsZXYdU4wvC"
+                $0.clientKey = "mmT02dB4wySoaLpzPKqJsxfVYony0sisSIfDJNVs"
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        // --- end copy
         return true
     }
 
